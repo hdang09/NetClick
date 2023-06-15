6d9745dffe056a0b812c0df36058978ad140f9bc
hdang09 <dangtranhai628@gmail.com>
2023-06-15 12:50:26 +0700

    UPDATE - Remove unnecessary code in MovieDAO

--- src/main/java/dao/MovieDAO.java
+++ src/main/java/dao/MovieDAO.java
@@ -109,37 +109,11 @@ public class MovieDAO {
     }
     
     public ArrayList<MovieDTO> getRelatedByTag(int id, String tag) {
-                ArrayList<MovieDTO> movies = new ArrayList<>();
-
-        String sql = "SELECT * FROM movie m, tag t, MovieTag mt "
-                + "where m.id = mt.movie_id and t.id = mt.tag_id and tag_name = ?";
-        try {
-            Connection conn = DBUtils.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, tag);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                String title = rs.getString("title");
-                String description = rs.getString("description");
-                String thumbnail = rs.getString("thumbnail");
-                String movieUrl = rs.getString("movie_url");
-                String release = rs.getString("release");
-                String director = rs.getString("director");
-                int rating = rs.getInt("rating");
-                // ArrayList tags = new ArrayList();
-                MovieDTO movie = new MovieDTO(id, title, description, thumbnail, movieUrl, LocalDate.MAX, director, rating, director);
-                movies.add(movie);
-            }
-            return movies;
-        } catch (SQLException ex) {
-            Logger.getLogger(MovieDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+        // TODO: Add this function
         return null;
     }
 
     public void delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // TODO: Add delete function
     }
 }
