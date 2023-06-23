80529bf0b7904ca43e12752748f1386bf3e8066e
hdang09 <dangtranhai628@gmail.com>
2023-06-23 22:39:36 +0700

    FIX - Fix redirect route to correct role of an account

--- src/main/java/controllers/AuthController.java
+++ src/main/java/controllers/AuthController.java
@@ -1,7 +1,7 @@
 /*
 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
-*/
+ */
 package controllers;
 
 import dao.AccountDAO;
@@ -16,20 +16,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
-*
-* @author Admin
-*/
+ *
+ * @author Admin
+ */
 public class AuthController extends HttpServlet {
 
-/**
-* Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-* methods.
-*
-* @param request servlet request
-* @param response servlet response
-* @throws ServletException if a servlet-specific error occurs
-* @throws IOException if an I/O error occurs
-*/
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -38,7 +38,7 @@ public class AuthController extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AuthController</title>");            
+            out.println("<title>Servlet AuthController</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet AuthController at " + request.getContextPath() + "</h1>");
@@ -49,59 +49,67 @@ public class AuthController extends HttpServlet {
 
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-}
+            throws ServletException, IOException {
+    }
 
     /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
 //        LOGIN
-            String username = request.getParameter("user");
-            String password = request.getParameter("pass");
+        String username = request.getParameter("user");
+        String password = request.getParameter("pass");
+
+        if (username.isEmpty() || password.isEmpty()) {
+            request.setAttribute("fill", "Please fill in all the fields");
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/login.jsp");
+            dispatch.forward(request, response);
+        }
+        AccountDAO accountdao = new AccountDAO();
+        AccountDTO user = accountdao.login(username, password);
+        if (user != null) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("tendangnhap", username);
             
-            if (username.isEmpty() || password.isEmpty()) {
-                request.setAttribute("fill", "Please fill in all the fields");
-                RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/login.jsp");
-                dispatch.forward(request, response);
+            // Role: admin
+            if (user.getRole() == 0) {
+                response.sendRedirect("/admin");
+                return;
             }
-            AccountDAO accountdao = new AccountDAO();
-            AccountDTO user = accountdao.login(username, password);
-                if (user != null) {
-                    HttpSession session = request.getSession(true);
-                    session.setAttribute("tendangnhap", username);
-                    RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/index.jsp");
-                    dispatch.forward(request, response);
-            } else {
-                    request.setAttribute("mess", "Wrong user or password");
-                    RequestDispatcher dispatch= getServletContext().getRequestDispatcher("/login.jsp");
-                    dispatch.forward(request, response);
-            }
+            
+            // Role: user
+            response.sendRedirect("/");
+        } else {
+            request.setAttribute("mess", "Wrong user or password");
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/login.jsp");
+            dispatch.forward(request, response);
+        }
     }
+
     /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
-    return "Short description";
+        return "Short description";
     }// </editor-fold>
 
 }
