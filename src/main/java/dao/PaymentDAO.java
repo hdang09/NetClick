2c139cb915051f214c509f99fda7b50a31eca8ee
hdang09 <dangtranhai628@gmail.com>
2023-06-28 00:12:02 +0700

    FIX - Fix SQL date in PaymentDAO (add funciton in DateUtils)

--- src/main/java/dao/PaymentDAO.java
+++ src/main/java/dao/PaymentDAO.java
@@ -12,38 +12,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import utils.DBUtils;
+import utils.DateUtils;
 
 /**
  *
  * @author Quan
  */
 public class PaymentDAO {
-//VISA
-     public PaymentDTO checkPaymentExist(Integer visa, Date expiredate, Integer cvv, String cholder) {
-        String sql = "SELECT *\n" +
-        "FROM payment\n" +
-        "WHERE visa_num = ? AND expire_visa_date = ? AND cvv = ? AND placeholder_card = ?";
-        
+
+    DateUtils dateUtils = new DateUtils();
+    
+    //VISA
+    public PaymentDTO checkPaymentExist(Integer visa, Date expiredate, Integer cvv, String cholder) {
+        String sql = "SELECT *\n"
+                + "FROM payment\n"
+                + "WHERE visa_num = ? AND expire_visa_date = ? AND cvv = ? AND placeholder_card = ?";
+
         try {
             Connection conn = DBUtils.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setInt(1, visa);
-            ps.setDate(2, (java.sql.Date) expiredate);
+            ps.setDate(2, dateUtils.convertToSqlDate(expiredate));
             ps.setInt(3, cvv);
             ps.setString(4, cholder);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 return new PaymentDTO(
-                    rs.getInt("id"),
-                    rs.getInt("userID"),
-                    rs.getInt("saNum"),
-                    rs.getDate("expireVisaDate"),
-                    rs.getInt("cvv"),
-                    rs.getString("placeholderCard"),
-                    rs.getInt("momoNum"),
-                    rs.getDate("startDate"),
-                    rs.getInt("subscriptionID")
-                                 
+                        rs.getInt("id"),
+                        rs.getInt("userID"),
+                        rs.getInt("saNum"),
+                        rs.getDate("expireVisaDate"),
+                        rs.getInt("cvv"),
+                        rs.getString("placeholderCard"),
+                        rs.getInt("momoNum"),
+                        rs.getDate("startDate"),
+                        rs.getInt("subscriptionID")
                 );
             }
         } catch (SQLException e) {
@@ -51,25 +54,27 @@ public class PaymentDAO {
         }
         return null;
     }
-        public void insertv(Integer visa, Date expiredate, Integer cvv, String cholder) {
-        String sql = "INSERT INTO payment (visa_num, expire_visa_date, cvv, placeholder_card)\n" +
-             "VALUES (?, ?, ?, ?)";
+
+    public void insertv(Integer visa, Date expiredate, Integer cvv, String cholder) {
+        String sql = "INSERT INTO Payment (visa_num, expire_visa_date, cvv, placeholder_card)\n"
+                + "VALUES (?, ?, ?, ?)";
         try {
             Connection conn = DBUtils.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setInt(1, visa);
-            ps.setDate(2, (java.sql.Date) expiredate);
+            ps.setDate(2, dateUtils.convertToSqlDate(expiredate));
             ps.setInt(3, cvv);
             ps.setString(4, cholder);
             ps.executeUpdate();
         } catch (SQLException e) {
         }
     }
-//MOMO
-        public void insertm(Integer momoNum) {
+    
+    //MOMO
+    public void insertm(Integer momoNum) {
         String sql = "INSERT INTO payment (visa_num, expire_visa_date, cvv,"
-                + " placeholder_card, momo_num, start_date, subscriptionID)\n" +
-                "VALUES (0, 0, 0, 0, ?, 0, 0)";
+                + " placeholder_card, momo_num, start_date, subscriptionID)\n"
+                + "VALUES (0, 0, 0, 0, ?, 0, 0)";
         try {
             Connection conn = DBUtils.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
@@ -78,33 +83,32 @@ public class PaymentDAO {
         } catch (SQLException e) {
         }
     }
-        
-        public PaymentDTO momoCheckExist(Integer momoNum) {
-            String sql = "SELECT *\n" +
-                    "FROM payment\n" +
-                    "WHERE momo_num = ?";
-            try {
-                Connection conn = DBUtils.getConnection();
-                PreparedStatement ps = conn.prepareStatement(sql);
-                ps.setInt(1, momoNum);
-                ResultSet rs = ps.executeQuery();
-                if (rs.next()) {
-                    return new PaymentDTO(
-                            rs.getInt("id"),
-                            rs.getInt("userID"),
-                            rs.getInt("saNum"),
-                            rs.getDate("expire_visa_date"),
-                            rs.getInt("cvv"),
-                            rs.getString("placeholderCard"),
-                            rs.getInt("momoNum"),
-                            rs.getDate("startDate"),
-                            rs.getInt("subscriptionID")
-                    );
-                }
-            } catch (SQLException e) {
+
+    public PaymentDTO momoCheckExist(Integer momoNum) {
+        String sql = "SELECT *\n"
+                + "FROM payment\n"
+                + "WHERE momo_num = ?";
+        try {
+            Connection conn = DBUtils.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, momoNum);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new PaymentDTO(
+                        rs.getInt("id"),
+                        rs.getInt("userID"),
+                        rs.getInt("saNum"),
+                        rs.getDate("expire_visa_date"),
+                        rs.getInt("cvv"),
+                        rs.getString("placeholderCard"),
+                        rs.getInt("momoNum"),
+                        rs.getDate("startDate"),
+                        rs.getInt("subscriptionID")
+                );
             }
-            return null;
-}
+        } catch (SQLException e) {
+        }
+        return null;
+    }
 
 }
-   
